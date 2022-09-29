f204d14e14c9f46873b9091daa0b0a2c1026990f
ido2103 <44828424+ido2103@users.noreply.github.com>
2022-09-29 11:20:22 +0300

    Update funcs.py

--- funcs.py
+++ funcs.py
@@ -1,10 +1,7 @@
-from multiprocessing import shared_memory
-from tkinter import Y
 import xlrd
+import random
 from xlrd import open_workbook
 from xlutils.copy import copy
-import time
-
 
 
 def avg(dict, key):
@@ -15,7 +12,7 @@ def avg(dict, key):
 
 
 def lowest(dict, key):
-    low = 0
+    low = 999
     for i in dict:
         if dict[i][key] < low and dict[i]["RestingHours"] <= 0:
             low = dict[i][key]
@@ -55,54 +52,115 @@ def listToDict(soldiers):
     return dict
 
 
-def reduceMitbahCD(dict, excel):
-    for i in dict:
-        if dict[i]["MitbahCooldown"] > 0:
-            dict[i]["MitbahCooldown"] -= 1
-
-
-def rewriteExcel(dict, excel):
-    for i in dict:
-        excel.write(dict[i]["Row"], 1, dict[i]["S.G"])
-        excel.write(int(dict[i]["Row"]), 2, dict[i]["Nishkia"])
-        excel.write(int(dict[i]["Row"]), 3, dict[i]["Siur"])
-        excel.write(int(dict[i]["Row"]), 4, dict[i]["Mitbach"])
-        excel.write(int(dict[i]["Row"]), 5, dict[i]["Hamal"])
-        excel.write(int(dict[i]["Row"]), 10, dict[i]["RestingHours"])
-        excel.write(int(dict[i]["Row"]), 12, dict[i]["MitbahCooldown"])
-
-
-def cycle(dict, cycleNum):
-    # Setting up the excel sheet. To write in it use excel.write(row, column, "data").
+def doSiur(dict):
     rb = open_workbook("shavtzak.xls")
-    wb = copy(rb)
-    excel = wb.get_sheet(0)
-    """TODO: create the function that is called upon 6 times in order to build the shavtzak. the function receives the list
-    of soldiers and builds each "phase" of the shavtzak according to the cycleNum when called. this is the annoying function
-    that will be very annoying to write.
-    Check for highest SG, Nishkia, siur and so on... Lastly place SG and Nishkia
-    The function finds the person with the highest amount of mitbach and then looks for people who are
-    a) can do mitbach
-    b) have a lower amount
-    * this can be improved because it's really memory inefficient but i'm lazy and i don't wanna do it right now
-    """
-    """The following section gets the 2 people that has the lowest amount to be in the kitchen. 
-    TODO:
-    make a 1 day minimum cooldown on mitbach"""
+    div1 = {}
+    div2 = {}
+    div3 = {}
+    div4 = {}
+    for i in dict:
+        value = rb.sheet_by_index(0).cell(int(dict[i]["Row"]), 3).value
+        j = {i: value}
+        if dict[i]["Division"] == 1 and dict[i]["RestingHours"] <= 0 and dict[i]["MitbahCooldown"] < 2:
+            div1.update(j)
+        elif dict[i]["Division"] == 2 and dict[i]["RestingHours"] <= 0 and dict[i]["MitbahCooldown"] < 2:
+            div2.update(j)
+        elif dict[i]["Division"] == 3 and dict[i]["RestingHours"] <= 0 and dict[i]["MitbahCooldown"] < 2:
+            div3.update(j)
+        elif dict[i]["Division"] == 4 and dict[i]["RestingHours"] <= 0 and dict[i]["MitbahCooldown"] < 2:
+            div4.update(j)
+    lowest1div1, lowest2div1 = 999, 999
+    soldier1div1, soldier2div1 = str, str
+    lowest1div2, lowest2div2 = 999, 999
+    soldier1div2, soldier2div2 = str, str
+    lowest1div3, lowest2div3 = 999, 999
+    soldier1div3, soldier2div3 = str, str
+    lowest1div4, lowest2div4 = 999, 999
+    soldier1div4, soldier2div4 = str, str
+    temp_list = []
+    list_of_soldiers = []
+    for i in div1:
+        if div1[i] < lowest1div1:
+            lowest1div1 = div1[i]
+            soldier1div1 = i
+        elif div1[i] < lowest2div1:
+            lowest2div1 = div1[i]
+            soldier2div1 = i
+        if lowest1div1 < lowest2div1:
+            lowest2div1, lowest1div1 = lowest1div1, lowest2div1
+            soldier2div1, soldier1div1 = soldier1div1, soldier2div1
+    temp_list.append(soldier1div1)
+    temp_list.append(soldier2div1)
+    list_of_soldiers.append(temp_list)
+    temp_list = []
+    for i in div2:
+        if div2[i] < lowest1div2:
+            lowest1div2 = div2[i]
+            soldier1div2 = i
+        elif div2[i] < lowest2div2:
+            lowest2div2 = div2[i]
+            soldier2div2 = i
+        if lowest1div2 < lowest2div2:
+            lowest2div2, lowest1div2 = lowest1div2, lowest2div2
+            soldier2div2, soldier1div2 = soldier1div2, soldier2div2
+    temp_list.append(soldier1div2)
+    temp_list.append(soldier2div2)
+    list_of_soldiers.append(temp_list)
+    temp_list = []
+    for i in div3:
+        if div3[i] < lowest1div3:
+            lowest1div3 = div3[i]
+            soldier1div3 = i
+        elif div3[i] < lowest2div3:
+            lowest2div3 = div3[i]
+            soldier2div3 = i
+        if lowest1div3 < lowest2div3:
+            lowest2div3, lowest1div3 = lowest1div3, lowest2div3
+            soldier2div3, soldier1div3 = soldier1div3, soldier2div3
+    temp_list.append(soldier1div3)
+    temp_list.append(soldier2div3)
+    list_of_soldiers.append(temp_list)
+    temp_list = []
+    for i in div4:
+        if div4[i] < lowest1div4:
+            lowest1div4 = div4[i]
+            soldier1div4 = i
+        elif div4[i] < lowest2div4:
+            lowest2div4 = div4[i]
+            soldier2div4 = i
+        if lowest1div4 < lowest2div4:
+            lowest2div4, lowest1div4 = lowest1div4, lowest2div4
+            soldier2div4, soldier1div4 = soldier1div4, soldier2div4
+    temp_list.append(soldier1div4)
+    temp_list.append(soldier2div4)
+    list_of_soldiers.append(temp_list)
+    for i in list_of_soldiers:
+        if i[0] == str or i[1] == "":
+            list_of_soldiers.remove(i)
+    r = random.randint(0, len(list_of_soldiers)-1)
+    for i in list_of_soldiers[r]:
+        for j in dict:
+            if i == j:
+                dict[j]["Siur"] += 1
+                dict[j]["RestingHours"] = 16
+
+    return list_of_soldiers[r]
+
+
+def doMitbah(dict):
+    rb = open_workbook("shavtzak.xls")
     highest1 = highest(dict, "Mitbach")
-    print(highest1)
     highest2 = highest1
     soldier1 = {}
     soldier2 = {}
     soldiers = []
-    t0 = time.perf_counter()
-    # Mitbah
-    # Mitbah
     for i in dict:
-        if dict[i]["Mitbach"] <= highest1 and (dict[i]["RestingHours"] <= 0) and (dict[i]["IsPtorMitbach"] == 0) and (dict[i]["MitbahCooldown"] <= 0):
+        if dict[i]["Mitbach"] <= highest1 and (dict[i]["RestingHours"] <= 0) and (dict[i]["IsPtorMitbach"] == 0) and (
+                dict[i]["MitbahCooldown"] <= 0):
             highest1 = dict[i]["Mitbach"]
             soldier1 = i
-        elif dict[i]["Mitbach"] <= highest2 and (dict[i]["RestingHours"] <= 0) and (dict[i]["IsPtorMitbach"] == 0) and (dict[i]["MitbahCooldown"] <= 0):
+        elif dict[i]["Mitbach"] <= highest2 and (dict[i]["RestingHours"] <= 0) and (dict[i]["IsPtorMitbach"] == 0) and (
+                dict[i]["MitbahCooldown"] <= 0):
             highest2 = dict[i]["Mitbach"]
             soldier2 = i
         if highest1 > highest2:
@@ -110,44 +168,90 @@ def cycle(dict, cycleNum):
             soldier1, soldier2 = soldier2, soldier1
     soldiers.append(soldier1)
     soldiers.append(soldier2)
-    print(soldiers)
     for i in dict:
         if i == soldier1:
             value = rb.sheet_by_index(0).cell(int(dict[i]["Row"]), 4).value
             value += 1
-            excel.write(int(dict[i]["Row"]), 4, value) # updating the mitbach count
-            dict[i]["MitbahCooldown"] = 3 # updating the mitbachcd
+            dict[i]["Mitbah"] = value  # updating the mitbach count
+            dict[i]["MitbahCooldown"] = 3  # updating the mitbachcd
+            dict[i]["RestingHours"] = 24
         elif i == soldier2:
             value = rb.sheet_by_index(0).cell(int(dict[i]["Row"]), 4).value
             value += 1
-            excel.write(int(dict[i]["Row"]), 4, value)
+            dict[i]["Mitbah"] = value
             dict[i]["MitbahCooldown"] = 3
+            dict[i]["RestingHours"] = 24
     for i in soldiers:
         for j in dict:
             if i == j:
                 dict[j]["Mitbach"] += 1
-    """
-    for i in range(len(soldiers)):
-        temp = {}
-        temp.update({soldiers[i]: "Mitbach"})
-        soldiers.append(temp)
-    """
-    soldiers = [soldier for soldier in soldiers if type(soldier) != str]
-    print(soldiers)
+
     for i in dict:
         if (dict[i]["IsPtorMitbach"] == 0) and (dict[i]["MitbahCooldown"] > 0):
-            print(dict[i]["MitbahCooldown"])
             dict[i]["MitbahCooldown"] -= 1
-            print(dict[i]["MitbahCooldown"])
-    # Siur
+    return soldiers, dict
+
+
+def doHamal(dict):
+    num = highest(dict, "Hamal")
+    soldier = ''
+    for i in dict:
+        if (dict[i]["RestingHours"] <= 0) and (dict[i]["IsHamal"] == 1) and (dict[i]["Hamal"] <= num):
+            num = dict[i]["Hamal"]
+            soldier = i
+    for i in dict:
+        if i == soldier:
+            dict[i]["RestingHours"] = 16
+            dict[i]["Hamal"] += 1
+    return soldier, dict
+
+
+def doSG(dict):
+    num = highest(dict, "S.G")
+    soldier = ''
+    for i in dict:
+        if (dict[i]["RestingHours"] <= 0) and (dict[i]["IsPtorShmirah"] == 0) and (dict[i]["S.G"] < num):
+            soldier = i
+            num = dict[i]["S.G"]
+    for i in dict:
+        if i == soldier:
+            dict[i]["RestingHours"] = 8
+            dict[i]["S.G"] += 1
+    return soldier, dict
+
+def rewriteExcel(dict):
+    for i in dict:
+        rb = open_workbook("shavtzak.xls")
+        wb = copy(rb)
+        excel = wb.get_sheet(0)
+        excel.write(dict[i]["Row"], 1, dict[i]["S.G"])
+        excel.write(int(dict[i]["Row"]), 2, dict[i]["Nishkia"])
+        excel.write(int(dict[i]["Row"]), 3, dict[i]["Siur"])
+        excel.write(int(dict[i]["Row"]), 4, dict[i]["Mitbach"])
+        excel.write(int(dict[i]["Row"]), 5, dict[i]["Hamal"])
+        excel.write(int(dict[i]["Row"]), 10, dict[i]["RestingHours"])
+        excel.write(int(dict[i]["Row"]), 12, dict[i]["MitbahCooldown"])
+        wb.save("shavtzak.xls")
+
+
+def cycle(dict):
+    # Setting up the excel sheet. To write in it use excel.write(row, column, "data").
+    rb = open_workbook("shavtzak.xls")
+    wb = copy(rb)
+    # Mitbah
+    mitbah, dict = doMitbah(dict)
+    print("Mitbah: ", ', '.join(mitbah))
     # Hamal
+    hamal, dict = doHamal(dict)
+    print("Hamal: ", hamal)
+    # Siur
+    siur = doSiur(dict)
+    print("Siur: ", ', '.join(siur))
     # Nishkia
     # SG
-    rewriteExcel(dict, excel)
-    wb.save("shavtzak.xls")
-    t1 = time.perf_counter()
-    print("Time:", t0-t1)
-    return soldiers
-
-
-
+    sg, dict = doSG(dict)
+    print("S.G: ", sg)
+    for i in dict:
+            dict[i]["RestingHours"] = 0
+    rewriteExcel(dict)
+    return dict
