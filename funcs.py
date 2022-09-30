70da55e7a72bc7be8da99df5a5663206e90b7c4e
ido2103 <44828424+ido2103@users.noreply.github.com>
2022-09-30 21:15:29 +0300

    Update funcs.py

--- funcs.py
+++ funcs.py
@@ -1,7 +1,11 @@
 import xlrd
+import xlwt
 import random
 from xlrd import open_workbook
 from xlutils.copy import copy
+from time import gmtime, strftime
+import os
+
 
 
 def avg(dict, key):
@@ -52,6 +56,75 @@ def listToDict(soldiers):
     return dict
 
 
+def makeExcel(list):
+    today = str(strftime("%Y-%m-%d", gmtime()))
+    book = xlwt.Workbook()
+    sheet = book.add_sheet("Shavtzak")
+    wb_name = "Shavtzak {0}".format(today)
+    list_of_doubled = []
+    all_people = []
+    style = xlwt.XFStyle()
+    font = xlwt.Font()
+    font.bold = True
+    style.font = font
+    style2 = xlwt.XFStyle()
+    font = xlwt.Font()
+    font.bold = False
+    style2.font = font
+    for i in list:
+        if type(i) == str:
+            all_people.append(i)
+        else:
+            all_people.append(i[0])
+            all_people.append(i[1])
+    for i in range(len(all_people)):
+        for j in range(1 + i, len(all_people)):
+            if all_people[i] == all_people[j]:
+                list_of_doubled.append(all_people[i])
+    sheet.write(0, 0, "Times: ")
+    sheet.write(0, 1, "S.G")
+    sheet.write(0, 2, "Tapuz")
+    sheet.write(0, 3, "Siur")
+    sheet.write(0, 4, "Hamal")
+    sheet.write(0, 5, "Mitbah")
+    sheet.write(1, 0, "6:00-10:00")
+    sheet.write(2, 0, "10:00-14:00")
+    sheet.write(3, 0, "14:00-18:00")
+    sheet.write(4, 0, "18:00-22:00")
+    sheet.write(5, 0, "22:00-2:00")
+    sheet.write(6, 0, "2:00-6:00")
+    sheet.write(1, 5, ", ".join(list[0]),
+                style=style if list[0][0] in list_of_doubled or list[0][1] in list_of_doubled else style2)
+    sheet.write(1, 4, list[1], style=style if list[1] in list_of_doubled else style2)
+    sheet.write(1, 3, ", ".join(list[2]),
+                style=style if list[2][0] in list_of_doubled or list[2][1] in list_of_doubled else style2)
+    sheet.write(1, 1, list[3], style=style if list[3] in list_of_doubled else style2)
+    sheet.write(1, 2, list[4], style=style if list[4] in list_of_doubled else style2)
+    sheet.write(2, 1, list[5], style=style if list[5] in list_of_doubled else style2)
+    sheet.write(2, 2, list[6], style=style if list[6] in list_of_doubled else style2)
+    sheet.write(3, 4, list[7], style=style if list[7] in list_of_doubled else style2)
+    sheet.write(3, 3, ", ".join(list[8]),
+                style=style if list[8][0] in list_of_doubled or list[8][1] in list_of_doubled else style2)
+    sheet.write(3, 1, list[9], style=style if list[9] in list_of_doubled else style2)
+    sheet.write(3, 2, list[10], style=style if list[10] in list_of_doubled else style2)
+    sheet.write(4, 1, list[11], style=style if list[11] in list_of_doubled else style2)
+    sheet.write(4, 2, list[12], style=style if list[12] in list_of_doubled else style2)
+    sheet.write(5, 4, list[13], style=style if list[13] in list_of_doubled else style2)
+    sheet.write(5, 3, ", ".join(list[14]), style=style if list[14][0] in list_of_doubled or list[14][1] else style2)
+    sheet.write(5, 1, list[15], style=style if list[15] in list_of_doubled else style2)
+    sheet.write(5, 2, list[16], style=style if list[16] in list_of_doubled else style2)
+    sheet.write(6, 1, list[17], style=style if list[17] in list_of_doubled else style2)
+    sheet.write(6, 2, list[18], style=style if list[18] in list_of_doubled else style2)
+    sheet.col(0).width = 3000
+    sheet.col(1).width = 3000
+    sheet.col(2).width = 5000
+    sheet.col(3).width = 8000
+    sheet.col(4).width = 5000
+    sheet.col(5).width = 8000
+    book.save(wb_name + ".xls")
+    return wb_name
+
+
 def doSiur(dict):
     rb = open_workbook("shavtzak.xls")
     div1 = {}
@@ -61,13 +134,13 @@ def doSiur(dict):
     for i in dict:
         value = rb.sheet_by_index(0).cell(int(dict[i]["Row"]), 3).value
         j = {i: value}
-        if dict[i]["Division"] == 1 and dict[i]["RestingHours"] <= 0 and dict[i]["MitbahCooldown"] < 2:
+        if dict[i]["Division"] == 1 and (dict[i]["RestingHours"] == 0 and (dict[i]["MitbahCooldown"] < 2)):
             div1.update(j)
-        elif dict[i]["Division"] == 2 and dict[i]["RestingHours"] <= 0 and dict[i]["MitbahCooldown"] < 2:
+        elif dict[i]["Division"] == 2 and (dict[i]["RestingHours"] == 0 and (dict[i]["MitbahCooldown"] < 2)):
             div2.update(j)
-        elif dict[i]["Division"] == 3 and dict[i]["RestingHours"] <= 0 and dict[i]["MitbahCooldown"] < 2:
+        elif dict[i]["Division"] == 3 and (dict[i]["RestingHours"] == 0 and (dict[i]["MitbahCooldown"] < 2)):
             div3.update(j)
-        elif dict[i]["Division"] == 4 and dict[i]["RestingHours"] <= 0 and dict[i]["MitbahCooldown"] < 2:
+        elif dict[i]["Division"] == 4 and (dict[i]["RestingHours"] == 0 and (dict[i]["MitbahCooldown"] < 2)):
             div4.update(j)
     lowest1div1, lowest2div1 = 999, 999
     soldier1div1, soldier2div1 = str, str
@@ -147,7 +220,6 @@ def doSiur(dict):
             if i == j:
                 dict[j]["Siur"] += 1
                 dict[j]["RestingHours"] = 16
-
     return list_of_soldiers[r], dict
 
 
@@ -191,7 +263,7 @@ def doMitbah(dict):
                 dict[j]["Mitbach"] += 1
 
     for i in dict:
-        if (dict[i]["IsPtorMitbach"] == 0) and (dict[i]["MitbahCooldown"] > 0):
+        if dict[i]["MitbahCooldown"] > 0:
             dict[i]["MitbahCooldown"] -= 1
     return soldiers, dict
 
@@ -200,7 +272,7 @@ def doHamal(dict):
     num = highest(dict, "Hamal")
     soldier = ''
     for i in dict:
-        if (dict[i]["RestingHours"] <= 0) and (dict[i]["IsHamal"] == 1) and (dict[i]["Hamal"] <= num):
+        if (dict[i]["RestingHours"] == 0) and (dict[i]["IsHamal"] == 1) and (dict[i]["Hamal"] <= num) and (dict[i]["MitbahCooldown"] < 2):
             num = dict[i]["Hamal"]
             soldier = i
     for i in dict:
@@ -214,12 +286,12 @@ def doSG(dict):
     num = highest(dict, "S.G")
     soldier = ''
     for i in dict:
-        if (dict[i]["RestingHours"] <= 0) and (dict[i]["IsPtorShmirah"] == 0) and (dict[i]["S.G"] <= num):
+        if (dict[i]["RestingHours"] == 0) and (dict[i]["IsPtorShmirah"] == 0) and (dict[i]["S.G"] <= num) and (dict[i]["MitbahCooldown"] < 2):
             soldier = i
             num = dict[i]["S.G"]
     for i in dict:
         if i == soldier:
-            dict[i]["RestingHours"] = 8
+            dict[i]["RestingHours"] = 12
             dict[i]["S.G"] += 1
     return soldier, dict
 
@@ -228,12 +300,12 @@ def doTapuz(dict):
     num = highest(dict, "Tapuz")
     soldier = ''
     for i in dict:
-        if (dict[i]["RestingHours"] <= 0) and (dict[i]["IsPtorShmirah"] == 0) and (dict[i]["Tapuz"] <= num):
+        if (dict[i]["RestingHours"] == 0) and (dict[i]["IsPtorShmirah"] == 0) and (dict[i]["Tapuz"] <= num) and (dict[i]["MitbahCooldown"] < 2):
             soldier = i
             num = dict[i]["Tapuz"]
     for i in dict:
         if i == soldier:
-            dict[i]["RestingHours"] = 8
+            dict[i]["RestingHours"] = 12
             dict[i]["Tapuz"] += 1
     return soldier, dict
 
@@ -253,99 +325,72 @@ def rewriteExcel(dict):
         wb.save("shavtzak.xls")
 
 
-def cycle2(dict): # New
-    #Mitbah
+def cycle(dict, siurimNum, hamalNum):
+    # TODO: MAKE SIURIMNUM AND HAMALNUM MEAN SOMETHING (REWRITE THE EXCEL.WRITE ALGORITHM)
+    # Mitbah
     list_of_soldiers = []
-    mitbah, dict = doMitbah(dict)
+    dict2 = {}
+    mitbah, dict2 = doMitbah(dict)
     list_of_soldiers.append(mitbah)
-    hamal, dict = doHamal(dict)
+    hamal, dict2 = doHamal(dict2)
     list_of_soldiers.append(hamal)
-    siur, dict = doSiur(dict)
+    siur, dict2 = doSiur(dict2)
     list_of_soldiers.append(siur)
-    sg, dict = doSG(dict)
+    sg, dict2 = doSG(dict2)
     list_of_soldiers.append(sg)
-    tapuz, dict = doTapuz(dict)
+    tapuz, dict2 = doTapuz(dict2)
     list_of_soldiers.append(tapuz)
-    for i in dict:
-        if dict[i]["RestingHours"] > 0:
-            dict[i]["RestingHours"] -= 4
-    sg, dict = doSG(dict)
+    for i in dict2:
+        if dict2[i]["RestingHours"] > 0:
+            dict2[i]["RestingHours"] -= 4
+    sg, dict2 = doSG(dict2)
     list_of_soldiers.append(sg)
-    tapuz, dict = doTapuz(dict)
+    tapuz, dict2 = doTapuz(dict2)
     list_of_soldiers.append(tapuz)
-    for i in dict:
-        if dict[i]["RestingHours"] > 0:
-            dict[i]["RestingHours"] -= 4
-    hamal, dict = doHamal(dict)
+    for i in dict2:
+        if dict2[i]["RestingHours"] > 0:
+            dict2[i]["RestingHours"] -= 4
+    hamal, dict2 = doHamal(dict2)
     list_of_soldiers.append(hamal)
-    siur, dict = doSiur(dict)
+    siur, dict2 = doSiur(dict2)
     list_of_soldiers.append(siur)
-    sg, dict = doSG(dict)
+    sg, dict2 = doSG(dict2)
     list_of_soldiers.append(sg)
-    tapuz, dict = doTapuz(dict)
+    tapuz, dict2 = doTapuz(dict2)
     list_of_soldiers.append(tapuz)
-    for i in dict:
-        if dict[i]["RestingHours"] > 0:
-            dict[i]["RestingHours"] -= 4
-    sg, dict = doSG(dict)
+    for i in dict2:
+        if dict2[i]["RestingHours"] > 0:
+            dict2[i]["RestingHours"] -= 4
+    sg, dict2 = doSG(dict2)
     list_of_soldiers.append(sg)
-    tapuz, dict = doTapuz(dict)
+    tapuz, dict2 = doTapuz(dict2)
     list_of_soldiers.append(tapuz)
-    for i in dict:
-        if dict[i]["RestingHours"] > 0:
-            dict[i]["RestingHours"] -= 4
-    hamal, dict = doHamal(dict)
+    for i in dict2:
+        if dict2[i]["RestingHours"] > 0:
+            dict2[i]["RestingHours"] -= 4
+    hamal, dict2 = doHamal(dict2)
     list_of_soldiers.append(hamal)
-    siur, dict = doSiur(dict)
+    siur, dict2 = doSiur(dict2)
     list_of_soldiers.append(siur)
-    sg, dict = doSG(dict)
+    sg, dict2 = doSG(dict2)
     list_of_soldiers.append(sg)
-    tapuz, dict = doTapuz(dict)
+    tapuz, dict2 = doTapuz(dict2)
     list_of_soldiers.append(tapuz)
-    for i in dict:
-        if dict[i]["RestingHours"] > 0:
-            dict[i]["RestingHours"] -= 4
-    sg, dict = doSG(dict)
+    for i in dict2:
+        if dict2[i]["RestingHours"] > 0:
+            dict2[i]["RestingHours"] -= 4
+    sg, dict2 = doSG(dict2)
     list_of_soldiers.append(sg)
-    tapuz, dict = doTapuz(dict)
+    tapuz, dict2 = doTapuz(dict2)
     list_of_soldiers.append(tapuz)
-    for i in dict:
-        if dict[i]["RestingHours"] > 0:
-            dict[i]["RestingHours"] -= 4
-        if i == sg or i == tapuz:
-            dict[i]["RestingHours"] = 8
-    rewriteExcel(dict)
+    for i in dict2:
+        if dict2[i]["RestingHours"] > 0:
+            dict2[i]["RestingHours"] -= 4
+    rewriteExcel(dict2)
+    excel_file = makeExcel(list_of_soldiers)
+    print("Succsfully made a new shavtzak.")
+    os.startfile(excel_file+".xls")
+
     return list_of_soldiers
 
 
-def cycle(dict):
-    # Setting up the excel sheet. To write in it use excel.write(row, column, "data").
-    rb = open_workbook("shavtzak.xls")
-    # Mitbah
-    mitbah, dict = doMitbah(dict)
-    print("Mitbah: ", ', '.join(mitbah))
-    # Hamal
-    hamalList = []
-    for i in range(3):
-        hamal, dict = doHamal(dict)
-        hamalList.append(hamal)
-    print("Hamal: ", ", ".join(hamalList))
-    # Siur
-    siurList = []
-    for i in range(2):
-        siur = doSiur(dict)
-        siurList.append(", ".join(siur))
-    print("Siur: ", ", ".join(siurList))
-    # SG & tapuz
-    sgList = []
-    tapuzList = []
-    for i in range(6):
-        sg, dict = doSG(dict)
-        tapuz, dict = doTapuz(dict)
-        sgList.append(sg)
-        tapuzList.append(tapuz)
-    print("S.G: ", ", ".join(sgList))
-    print("Tapuz: ", ", ".join(tapuzList))
-    rewriteExcel(dict)
-    # TODO: RESTING HOURS & UI AND WE'RE FUCKIN DONE WITH BACKEND LES GO(add option for 4/8 & weekend option)
-    return dict
