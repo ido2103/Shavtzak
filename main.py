b16ba2b7682f8054997f5cb5a0f948ef7ebd0586
ido2103 <44828424+ido2103@users.noreply.github.com>
2022-10-02 11:50:56 +0300

    Update main.py

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ def main():
         list_of_soldiers.append(soldier_value)
 
     dict = listToDict(list_of_soldiers)
-    # list = cycle(dict, 3, 3)
+    #list = cycle2(dict, 2, 1)
     """
     The code works in 6 cycles, updating the rest hours & current mission of each soldier on duty. This will be done by calling in a specific function several times.
     The aim is to make sure the soldiers don't do more than 4/8 or 8/8. Doing 4/8 reduces 3 resting points (1 for the 4 hours of guarding and 2 for the 8 hours of rest),
@@ -34,6 +34,7 @@ def main():
     """
     window(dict)
     print("Time: ", time.process_time() - time1)
+
 
 if __name__ == "__main__":
     try:
